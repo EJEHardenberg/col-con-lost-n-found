2d709bcd9c3efef46ed4fe9df9ee1ac48ab2397f
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-01-13 08:46:18 -0500

    Removed unneccesary form from featuretypeform
    Also moved models to shared directory since it makes more sense for it
    to be there.
    
    Original intent of featuretypeform was to make it possible to have the
    UI dynamically constructed by the feature types and entities given,
    kinda like a weird window manager in some ways. I'm going to focus on
    functionality first and then try to do this.

--- shared/featuretypeform.php
+++ shared/featuretypeform.php
@@ -10,25 +10,4 @@ if (get_class($feature) != 'FeatureType') {
 	return; //no
 }
 
-?><div>
-	<form class="grid-form">
-		<fieldset>
-			<legend>Feature Type</legend>
-			<div data-row-span="2">
-				<div data-field-span="1">
-					<label>Name</label>
-					<input type="text">
-				</div>
-				<div data-field-span="1">
-					<label>
-						<input type="radio" checked name="is_">Multiple Choice
-					</label>
-					<label>
-						<input type="radio" checked name="is_">Dropdown Option
-					</label>
-				</div>
-			</div>
-		</fieldset>
-		<input type="submit" class="button-green bigger" value="Create" />
-	</form>
-</div>
+?>
