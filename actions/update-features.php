f0144db03a3012594f4f9acc8b227f491f1258bc
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-01-16 18:30:34 -0500

    Working on update features. still needs work.
    added todo for the rest of it, but skeleton is in place.
    
    I think I'm going to want to refactor this afterwards mainly because all
    the updates actions function on the same principle, so I should be able
    to make a generic controller or something to use in the same way that
    the services could probably be generified.

--- actions/update-features.php
+++ actions/update-features.php
@@ -7,4 +7,52 @@ if (!isset($_POST['features']) || !is_array($_POST['features'])) {
 	send_failure_redirect('update-features');
 }
 
-echo '<pre>'; print_r($_POST); exit();
+/* Delete first so we don't do unneccesary updates  */
+if (isset($_POST['delete']) && is_array($_POST['delete'])) {
+	$allDeleted = FeatureService::deleteFeatures($_POST['delete']);
+	if (!$allDeleted) {
+		send_failure_redirect('update-events');
+	}
+}
+
+/* Update any features that require updating */
+$allUpdated = true;
+$features = FeatureService::getFeatures();
+$oldFeatures = array();
+foreach ($features as $feature) {
+	$oldFeatures[$feature->id] = $feature;
+}
+
+/* Do Stuff */
+foreach ($_POST['features'] as $featureArray) {
+	if (!isset($featureArray['id']) || !is_numeric($featureArray['id'])) {
+		continue;
+	}
+	if (!isset($featureArray['feature_type']) || !is_numeric($featureArray['feature_type'])) {
+		continue;
+	}
+	if (!isset($featureArray['name'])) {
+		continue;
+	}
+	$feature = new Feature();
+	$feature->id = $featureArray['id'];
+	$feature->name = $featureArray['name'];
+	$feature->feature_type = $featureArray['feature_type'];
+
+	if (!isset($oldFeatures[$feature->id])) {
+		continue;
+	}
+
+	$oldFeature = $oldFeatures[$feature->id]);
+	if ($oldFeature->name != $feature->name 				|| 
+		$oldFeature->feature_type != $feature->feature_type ){
+		//todo update
+	}
+
+}
+
+if (!$allUpdated) {
+	send_failure_redirect('update-events');
+}
+
+send_success_redirect('update-events');
