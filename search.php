fc982b658fb521d0fd545c16e46828b6004dfcb0
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-02-01 12:33:23 -0500

    Revert "Removing form from search page."
    This reverts commit 1e366f83c791b98fe524e368feca5d6ff9b19968.
    
    Reverted because it makes sense to search for an item then update it I
    suppose.

--- search.php
+++ search.php
@@ -37,13 +37,24 @@ foreach ($events as $event) {
 						</div>
 					</form>
 				</div>
+				<!-- Table andd all those wonderful things here -->
+				<form method="POST" action="/actions/update-items.php"  class="grid-form">
+					<?php
+					conditional_error_success(
+						'There was an issue updating all items',
+						'Successfully updated all items',
+						'update-items'
+					);
+					?>
 				<table class="flakes-table">
 					<colgroup>
+						<col span="1" style="20px">
 						<col span="1" style="20px">
 						<col span="1" style="20%">
 					</colgroup>
 					<thead>
 						<tr>
+							<th></th>
 							<th>Event</th>
 							<th>Name</th>
 							<th>Found</th>
@@ -54,14 +65,24 @@ foreach ($events as $event) {
 					<tbody>
 						<?php foreach ($items as $item) : ?>
 							<tr>
+								<td><input type="checkbox" name="delete[]" value="<?php echo $item->id ?>"></td>
 								<td><?php echo $eventsById[$item->event_id]->name; ?></td>
 								<td><?php echo $item->name; ?></td>
-								<td><?php echo $item->is_found ? 'Found' : 'Lost' ?> 
+								<td><label>is found &nbsp;
+									<input type="hidden" name="items[<?php echo $item->id; ?>][id]" value="<?php echo $item->id; ?>" />
+									<input 
+										type="checkbox" 
+										<?php echo $item->is_found ? 'checked' : '' ?> 
+										name="items[<?php echo $item->id; ?>][is_found]" 
+										value="<?php echo $item->id ?>">
+									</label>
 								</td>
 								<td><?php echo date('m/d/Y', strtotime($item->submitted_time)); ?></td>
 							</tr>
 						<?php endforeach; ?>
 					</tbody>
 				</table>
+					<input type="submit" class="button-blue" value="Delete/Update Selected" > 
+				</form>
 			</div>
 		</div><!-- ends header started div. -->
